92c45f079eec2674f5c442c56bb3d9897fc6804f
Mika Muurinen <mika.t.muurinen@gmail.com>
2019-07-07 17:27:59 +0300

    Initial commit

--- app/src/main/java/com/example/restapplication/MainActivity.java
+++ app/src/main/java/com/example/restapplication/MainActivity.java
@@ -9,7 +9,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.lang.String;
 
 
 public class MainActivity extends AppCompatActivity {
