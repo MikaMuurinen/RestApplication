072c4f0c37b212427718fafea6dc62cd325b5490
mika.t.muurinen@gmail.com <muumik65!>
2019-07-07 17:01:18 +0300

    Initial commit

---
+++ app/src/main/java/com/example/restapplication/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.restapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.StrictMode;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (android.os.Build.VERSION.SDK_INT > 9)
+        {
+            StrictMode.ThreadPolicy policy = new
+                    StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+
+        try
+        {
+            System.out.println("MyGETRequest");
+            MyGETRequest();
+        }
+        catch(IOException e)
+        {
+            System.out.println("Caught inside fun().");
+        }
+
+
+    }
+
+
+    public static void MyGETRequest() throws IOException {
+        URL urlForGetRequest = new URL("http://www.finnkino.fi/xml");
+        String readLine = null;
+        HttpURLConnection conection = (HttpURLConnection) urlForGetRequest.openConnection();
+        System.out.println("Connection");
+        conection.setRequestMethod("GET");
+        conection.setRequestProperty("userId", "a1bcdef"); // set userId its a sample here
+        int responseCode = conection.getResponseCode();
+
+        System.out.println("Response code:"+responseCode);
+
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(conection.getInputStream()));
+            StringBuffer response = new StringBuffer();
+            while ((readLine = in.readLine()) != null) {
+                response.append(readLine);
+            }
+            in.close();
+            // print result
+            System.out.println("JSON String Result " + response.toString());
+            //GetAndPost.POSTRequest(response.toString());
+        } else {
+            System.out.println("GET NOT WORKED");
+        }
+    }
+
+}
