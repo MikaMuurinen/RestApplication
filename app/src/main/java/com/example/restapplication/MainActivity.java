acf91fcdbe8144c8a9a53e610f7135548b83ae21
Mika Muurinen <mika.t.muurinen@gmail.com>
2019-07-07 17:10:17 +0300

    Initial commit

--- app/src/main/java/com/example/restapplication/MainActivity.java
+++ app/src/main/java/com/example/restapplication/MainActivity.java
@@ -7,8 +7,6 @@ import android.os.StrictMode;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
