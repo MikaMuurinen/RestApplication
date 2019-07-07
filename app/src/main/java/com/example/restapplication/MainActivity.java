2d06ffb94289e1e84f8e2439fa4963aba51299b4
Mika Muurinen <mika.t.muurinen@gmail.com>
2019-07-07 17:06:46 +0300

    Initial commit

--- app/src/main/java/com/example/restapplication/MainActivity.java
+++ app/src/main/java/com/example/restapplication/MainActivity.java
@@ -11,7 +11,6 @@ import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
-import java.net.ProtocolException;
 import java.net.URL;
 
 
